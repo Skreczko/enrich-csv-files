465d46ae1a596a882277366efc5d5d5004f24cb7
Skreczko <dawid.skreczko@gmail.com>
2023-08-26 23:13:37 +0200

    frontend-2 wip UploadFile.tsx

--- frontend/static/js/router/Router.tsx
+++ frontend/static/js/router/Router.tsx
@@ -2,11 +2,11 @@ import React, { ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import routes from './config/routes';
 import { Test1 } from '../components/body/Test1';
-import { Test2 } from '../components/body/Test2';
+import { UploadFile } from '../components/body/UploadFile';
 
 export const Router = (): ReactElement => (
   <Routes>
     <Route index element={<Test1 />} />
-    <Route path={routes.UPLOAD} element={<Test2 />} />
+    <Route path={routes.UPLOAD} element={<UploadFile />} />
   </Routes>
 );
