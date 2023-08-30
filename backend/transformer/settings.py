26690770830bed8848c4bd1af8205d5a17709d4c
Skreczko <dawid.skreczko@gmail.com>
2023-08-31 01:08:11 +0200

    backend-3 test mypy on github

--- backend/transformer/settings.py
+++ backend/transformer/settings.py
@@ -23,7 +23,7 @@ MYPY = "mypy" in sys.argv[0]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+TEST: str = 1
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
