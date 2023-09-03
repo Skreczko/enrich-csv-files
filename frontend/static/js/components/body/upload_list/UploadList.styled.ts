61103c37b7d9d14d10facc4cd3df117c3391cdea
Skreczko <dawid.skreczko@gmail.com>
2023-09-04 01:42:17 +0200

    frontend-5 WIP UploadList.tsx

---
+++ frontend/static/js/components/body/upload_list/UploadList.styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const UploadListWrapper = styled.div`
+  height: 2000px;
+  width: 2000px;
+`;
---
+++ frontend/static/js/components/body/upload_list/UploadList.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { UploadListWrapper } from './UploadList.styled';
+
+export const UploadList: React.FC = () => (
+  <UploadListWrapper>Test1 Test1 Test1 Test1</UploadListWrapper>
+);
