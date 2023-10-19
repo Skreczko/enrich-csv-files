6e9b432fd81580ed994b3d717ce8f2747639b749
Skreczko <dawid.skreczko@gmail.com>
2023-10-20 00:36:20 +0200

    frontend-12 custom hook render function - not working correctly - to fix in future development

--- frontend/static/js/utils/testing-utils.tsx
+++ frontend/static/js/utils/testing-utils.tsx
@@ -1,11 +1,18 @@
 import React, { Dispatch, ReactElement } from 'react';
-import { render as rtlRender, RenderResult } from '@testing-library/react';
-import { AnyAction, configureStore } from '@reduxjs/toolkit';
+import { Queries, render as rtlRender, RenderResult } from '@testing-library/react';
+import { AnyAction, configureStore, ThunkMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { ValidateSliceCaseReducers } from '@reduxjs/toolkit/dist/createSlice';
 import { RootState, storeReducer } from '../redux/store';
 import { MemoryRouter as Router } from 'react-router-dom';
+import {
+  useFetchPreviewChunk,
+  UseFetchPreviewChunkResult,
+} from '../components/hooks/useFetchPreviewChunk';
+import { selectedCsvFileSourceUuid } from './mockData';
+import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
+import { renderHook, WaitForNextUpdate } from '@testing-library/react-hooks';
 
 interface RenderOptions {
   preloadedState?: RootState;
@@ -47,8 +54,31 @@ function render(
 
   return { store, renderResult };
 }
-
 // re-export everything
 export * from '@testing-library/react';
 // override render method
 export { render };
+
+// CUSTOM HOOK RENDER
+// todo not working correctly. To fix in future development
+export const prepareRenderHook = <HookResult extends Queries, HookArgs extends any[]>(
+  customHookFactory: (...args: HookArgs) => () => HookResult,
+  ...hookArgs: HookArgs
+): {
+  store: ToolkitStore<any, any, [ThunkMiddleware<any, AnyAction, undefined>]>;
+  result: RenderResult<HookResult>;
+  waitForNextUpdate: WaitForNextUpdate;
+} => {
+  const store = configureStore({
+    reducer: storeReducer,
+  });
+
+  const customHook = customHookFactory(...hookArgs);
+
+  const { result, waitForNextUpdate } = renderHook<{}, HookResult>(customHook, {
+    wrapper: ({ children }) => <Provider store={store}>{children}</Provider>,
+  });
+
+  // @ts-ignore
+  return { store, result, waitForNextUpdate };
+};
