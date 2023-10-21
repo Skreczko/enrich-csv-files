6bba32178a74d2ea4c0f5d39d2d9603f1802ea91
Skreczko <dawid.skreczko@gmail.com>
2023-10-21 15:51:25 +0200

    frontend-12 useTaskDispatcher.test.tsx

--- frontend/static/js/components/hooks/__tests__/useTaskDispatcher.test.tsx
+++ frontend/static/js/components/hooks/__tests__/useTaskDispatcher.test.tsx
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux';
-import { useFetchPreviewChunk, UseFetchPreviewChunkResult } from '../useFetchPreviewChunk';
-import { act, renderHook, RenderResult, WaitForNextUpdate } from '@testing-library/react-hooks';
+import { act, renderHook, WaitForNextUpdate } from '@testing-library/react-hooks';
 import { storeReducer } from '../../../redux/store';
 import { AnyAction, configureStore, ThunkMiddleware } from '@reduxjs/toolkit';
 import React from 'react';
@@ -9,13 +8,10 @@ import {
   csvElementDetailEnrichStep1,
   csvElementDetailEnrichStep2,
   csvElementDetailFetched,
-  csvElementDetailNotFetched,
-  fileHeaders,
-  selectedCsvFileSourceUuid,
   selectedCsvFileUuid,
 } from '../../../utils/mockData';
 import { setupServer } from 'msw/node';
-import { rest } from 'msw';
+import { MockedResponse, ResponseTransformer, rest, RestContext } from 'msw';
 import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
 import { useTaskDispatcher } from '../useTaskDispatcher';
 import { setTask } from '../../../redux/TaskListReducer';
@@ -23,8 +19,13 @@ import { setFileList, setPaginator } from '../../../redux/FileListSlice';
 import { TaskResultFailure, TaskResultPending, TaskResultSuccess } from '../../../api/types';
 import { CeleryTaskStatus } from '../../../api/enums';
 import { waitFor } from '@testing-library/react';
+import { NotificationAppearanceEnum } from '../../notification/NotificationPopup';
+import { MaybePromise } from '@reduxjs/toolkit/dist/query/tsHelpers';
 
 const taskId = '8f3d2f80-6647-496c-9dab-0123b73f6da6';
+
+// variable used to change mocked response from taskResultPending(1st request)
+// to taskResultSuccess/taskResultFailure (2nd request)
 let requestCount = 0;
 
 const taskResultFailure: Record<string, TaskResultFailure> = {
@@ -49,6 +50,21 @@ const taskResultSuccess: Record<string, TaskResultSuccess> = {
   },
 };
 
+const fetchTaskResultResponse = (
+  res: (...transformers: ResponseTransformer<any>[]) => MaybePromise<MockedResponse<any>>,
+  ctx: RestContext,
+  isSuccess = true,
+): Promise<MockedResponse<any>> => {
+  return new Promise(resolve => {
+    if (requestCount >= 1) {
+      resolve(res(ctx.status(200), ctx.json(isSuccess ? taskResultSuccess : taskResultFailure)));
+    } else {
+      requestCount += 1;
+      resolve(res(ctx.status(200), ctx.json(taskResultPending)));
+    }
+  });
+};
+
 const server = setupServer(
   rest.get('/api/_internal/csv_list', (req, res, ctx) =>
     res(
@@ -63,8 +79,9 @@ const server = setupServer(
     return res(ctx.status(200), ctx.json(csvElementDetailFetched));
   }),
   rest.post('/api/_internal/fetch_task_results', (req, res, ctx) => {
-    if (requestCount > 3) {
-      return res(ctx.status(200), ctx.json(taskResultSuccess));
+    // to check if request will be refetch again including timeout dispatch
+    if (requestCount >= 1) {
+      return fetchTaskResultResponse(res, ctx);
     } else {
       requestCount += 1;
       return res(ctx.status(200), ctx.json(taskResultPending));
@@ -88,6 +105,11 @@ const prepareRenderHook = (): {
   const store = configureStore({
     reducer: storeReducer,
   });
+  act(() => {
+    store.dispatch(setTask({ [taskId]: { instance: 'CsvFile', uuid: selectedCsvFileUuid } }));
+    store.dispatch(setPaginator(basicUploadList.paginator));
+    store.dispatch(setFileList([csvElementDetailEnrichStep2, ...basicUploadList.result]));
+  });
 
   const { result, waitForNextUpdate } = renderHook<
     {},
@@ -104,77 +126,57 @@ const prepareRenderHook = (): {
 };
 
 describe('useTaskDispatcher', () => {
-  test('Load more preview chunks when scrolling', async () => {
+  beforeEach(() => {
+    // reset request counter
+    requestCount = 0;
+  });
+  test.each([
+    ['taskResultSuccess', true],
+    ['taskResultFailure', false],
+  ])('Celery taskResultPending and then %s', async (_, isSuccess: boolean) => {
+    if (!isSuccess) {
+      server.use(
+        rest.post('/api/_internal/fetch_task_results', (req, res, ctx) => {
+          // to check if request will be refetch again including timeout dispatch
+          if (requestCount >= 1) {
+            return fetchTaskResultResponse(res, ctx, isSuccess);
+          } else {
+            requestCount += 1;
+            return res(ctx.status(200), ctx.json(taskResultPending));
+          }
+        }),
+      );
+    }
+
     const { store, result, waitForNextUpdate } = prepareRenderHook();
 
-    act(() => {
-      store.dispatch(setPaginator(basicUploadList.paginator));
-      store.dispatch(setFileList([csvElementDetailEnrichStep2, ...basicUploadList.result]));
-    });
-    act(() => {
-      store.dispatch(setTask({ [taskId]: { instance: 'CsvFile', uuid: selectedCsvFileUuid } }));
-    });
+    // check if notifications are empty
+    expect(store.getState().notificationPopup).toHaveLength(0);
 
-    result.timeoutTaskDispatcher([taskId]);
-
-    // await waitForNextUpdate({ timeout: 10000 });
+    await act(async () => {
+      // run hook
+      result.timeoutTaskDispatcher([taskId]);
+      // wait for async actions (fetch with pending -> timeout -> fetch with success -> redux update). 10s just in case.
+      await waitForNextUpdate({ timeout: 10000 });
+    });
 
     await waitFor(() => {
-      expect(store.getState().taskList).not.toEqual({});
+      // task list should be empty -> cleared after successful request
+      expect(store.getState().taskList).toEqual({});
+      // notification should emerge
+      expect(store.getState().notificationPopup).toHaveLength(1);
     });
-    expect(store.getState().fileList.fileList[0]).toEqual({});
+    // check notification status
+    expect(store.getState().notificationPopup[0]['appearance']).toEqual(
+      isSuccess ? NotificationAppearanceEnum.SUCCESS : NotificationAppearanceEnum.ERROR,
+    );
 
-    // // at initial run - 200 rows should be fetched
-    // const initialDataLength = result.current.foundPreviewDetail.rows.length;
-    // expect(result.current.foundPreviewDetail.rows.length).toEqual(initialDataLength);
-    // // check redux state
-    // expect(store.getState().previewList).toEqual({
-    //   [selectedCsvFileSourceUuid]: {
-    //     chunkSize: 200,
-    //     headers: fileHeaders,
-    //     lastChunkNumber: 0,
-    //     rows: generateRows(200),
-    //     totalRows: 400,
-    //   },
-    // });
-    //
-    // // scrolling to reach 80% of total rows - that should trigger fetching another 200 rows (400 total)
-    // act(() => {
-    //   result.current.handleScrollDebounced({
-    //     visibleRowStartIndex: 0.7 * initialDataLength,
-    //     visibleRowStopIndex: 0.8 * initialDataLength,
-    //   });
-    // });
-    //
-    // await waitForNextUpdate();
-    //
-    // const secondFetchDataLength = 400;
-    // expect(result.current.foundPreviewDetail.rows.length).toEqual(secondFetchDataLength);
-    // // check redux state
-    // const secondFetchDataReduxState = {
-    //   [selectedCsvFileSourceUuid]: {
-    //     chunkSize: 200,
-    //     headers: fileHeaders,
-    //     lastChunkNumber: 1,
-    //     rows: generateRows(400),
-    //     totalRows: 400,
-    //   },
-    // };
-    // expect(store.getState().previewList).toEqual(secondFetchDataReduxState);
-    //
-    // // scrolling to reach 80% of total rows - fetching should not run, as totalRows has been reached
-    // act(() => {
-    //   result.current.handleScrollDebounced({
-    //     visibleRowStartIndex: 0.7 * secondFetchDataLength,
-    //     visibleRowStopIndex: 0.8 * secondFetchDataLength,
-    //   });
-    // });
-    //
-    // await waitForNextUpdate();
-    //
-    // // reached totalRows - 3rd fetch should not be triggered
-    // expect(result.current.foundPreviewDetail.rows.length).toEqual(secondFetchDataLength);
-    // // check redux state
-    // expect(store.getState().previewList).toEqual(secondFetchDataReduxState);
+    if (isSuccess) {
+      // check if related csvFile has been updated
+      expect(store.getState().fileList.fileList[0]).toEqual(csvElementDetailFetched);
+    } else {
+      // check if related csvFile has not been updated
+      expect(store.getState().fileList.fileList[0]).not.toEqual(csvElementDetailFetched);
+    }
   });
 });
